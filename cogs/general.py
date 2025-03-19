3b096f326c23d79546741f5b3d89a06dab0e16d9
blacksmithop <angstycoder101@gmaii.com>
2025-03-19 12:57:41 +0530

    feat: Cleanup

--- cogs/general.py
+++ cogs/general.py
@@ -1,7 +1,4 @@
 import platform
-import random
-
-import aiohttp
 import discord
 from discord import app_commands
 from discord.ext import commands
@@ -26,54 +23,6 @@ class FeedbackForm(discord.ui.Modal, title="Feeedback"):
 class General(commands.Cog, name="general"):
     def __init__(self, bot) -> None:
         self.bot = bot
-        self.context_menu_user = app_commands.ContextMenu(
-            name="Grab ID", callback=self.grab_id
-        )
-        self.bot.tree.add_command(self.context_menu_user)
-        self.context_menu_message = app_commands.ContextMenu(
-            name="Remove spoilers", callback=self.remove_spoilers
-        )
-        self.bot.tree.add_command(self.context_menu_message)
-
-    # Message context menu command
-    async def remove_spoilers(
-        self, interaction: discord.Interaction, message: discord.Message
-    ) -> None:
-        """
-        Removes the spoilers from the message. This command requires the MESSAGE_CONTENT intent to work properly.
-
-        :param interaction: The application command interaction.
-        :param message: The message that is being interacted with.
-        """
-        spoiler_attachment = None
-        for attachment in message.attachments:
-            if attachment.is_spoiler():
-                spoiler_attachment = attachment
-                break
-        embed = discord.Embed(
-            title="Message without spoilers",
-            description=message.content.replace("||", ""),
-            color=0xBEBEFE,
-        )
-        if spoiler_attachment is not None:
-            embed.set_image(url=attachment.url)
-        await interaction.response.send_message(embed=embed, ephemeral=True)
-
-    # User context menu command
-    async def grab_id(
-        self, interaction: discord.Interaction, user: discord.User
-    ) -> None:
-        """
-        Grabs the ID of the user.
-
-        :param interaction: The application command interaction.
-        :param user: The user that is being interacted with.
-        """
-        embed = discord.Embed(
-            description=f"The ID of {user.mention} is `{user.id}`.",
-            color=0xBEBEFE,
-        )
-        await interaction.response.send_message(embed=embed, ephemeral=True)
 
     @commands.hybrid_command(
         name="help", description="List all commands the bot has loaded."
@@ -126,37 +75,6 @@ class General(commands.Cog, name="general"):
         await context.send(embed=embed)
 
     @commands.hybrid_command(
-        name="serverinfo",
-        description="Get some useful (or not) information about the server.",
-    )
-    async def serverinfo(self, context: Context) -> None:
-        """
-        Get some useful (or not) information about the server.
-
-        :param context: The hybrid command context.
-        """
-        roles = [role.name for role in context.guild.roles]
-        num_roles = len(roles)
-        if num_roles > 50:
-            roles = roles[:50]
-            roles.append(f">>>> Displaying [50/{num_roles}] Roles")
-        roles = ", ".join(roles)
-
-        embed = discord.Embed(
-            title="**Server Name:**", description=f"{context.guild}", color=0xBEBEFE
-        )
-        if context.guild.icon is not None:
-            embed.set_thumbnail(url=context.guild.icon.url)
-        embed.add_field(name="Server ID", value=context.guild.id)
-        embed.add_field(name="Member Count", value=context.guild.member_count)
-        embed.add_field(
-            name="Text/Voice Channels", value=f"{len(context.guild.channels)}"
-        )
-        embed.add_field(name=f"Roles ({len(context.guild.roles)})", value=roles)
-        embed.set_footer(text=f"Created at: {context.guild.created_at}")
-        await context.send(embed=embed)
-
-    @commands.hybrid_command(
         name="ping",
         description="Check if the bot is alive.",
     )
@@ -185,26 +103,6 @@ class General(commands.Cog, name="general"):
         """
         embed = discord.Embed(
             description=f"Invite me by clicking [here]({self.bot.config['invite_link']}).",
-            color=0xD75BF4,
-        )
-        try:
-            await context.author.send(embed=embed)
-            await context.send("I sent you a private message!")
-        except discord.Forbidden:
-            await context.send(embed=embed)
-
-    @commands.hybrid_command(
-        name="server",
-        description="Get the invite link of the discord server of the bot for some support.",
-    )
-    async def server(self, context: Context) -> None:
-        """
-        Get the invite link of the discord server of the bot for some support.
-
-        :param context: The hybrid command context.
-        """
-        embed = discord.Embed(
-            description=f"Join the support server for the bot by clicking [here](https://discord.gg/mTBrXyWxAF).",
             color=0xD75BF4,
         )
         try:
