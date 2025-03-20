43387465a3d2dc3049c7361942e8c7a91fb0060a
blacksmithop <angstycoder101@gmaii.com>
2025-03-20 12:29:28 +0530

    add typing status while generating summary

--- cogs/nlp.py
+++ cogs/nlp.py
@@ -1,7 +1,8 @@
 from discord.ext import commands
 from discord.ext.commands import Context
-from utils import get_summary, LangchainFailiure
+from utils import summarize_text, LangchainFailiure
 from discord import Embed
+
 
 class NLP(commands.Cog, name="nlp"):
     def __init__(self, bot) -> None:
@@ -11,23 +12,22 @@ class NLP(commands.Cog, name="nlp"):
         name="summarize",
         description="Summarize text.",
     )
-    async def summarize(self, context: Context, *, text: str ) -> None:
+    async def summarize(self, context: Context, *, text: str) -> None:
         """
         This is a testing command that does nothing.
 
         :param context: The application command context.
         """
         try:
-            summary = get_summary(text=text)
-            embed = Embed(
-            description=f"Summary: {summary}", color=0xE02B2B
-        )
-            await context.send(embed=embed)
+            async with context.typing():
+                summary = summarize_text(text=text)
+                embed = Embed(description=summary, color=0xE02B2B)
+                await context.send(embed=embed)
         except LangchainFailiure:
-            embed = Embed(
-            description=f"Summary: {summary}", color=0xE02B2B
-            )
-            return "Failed to summarize text"
+            embed = Embed(description="Failed to summarize text", color=0xE02B2B)
+            await context.send(embed=embed)
+
+
 
 async def setup(bot) -> None:
     await bot.add_cog(NLP(bot))
