395f7ea73673141c1cab855f382fcbcf5a110ef0
blacksmithop <angstycoder101@gmaii.com>
2025-03-20 12:29:52 +0530

    feat: ConfigurableField for LLM parameters

--- utils/llm_core.py
+++ utils/llm_core.py
@@ -1,9 +1,11 @@
 from os import getenv
-from utils import llm_provider, llm_config
+from utils import llm_provider, llm_params, llm_config
 from typing import Union
 from utils import GeminiConfig, OllamaConfig, OpenAIConfig
 
-llm_config : Union[GeminiConfig, OllamaConfig, OpenAIConfig]
+
+llm_params: Union[GeminiConfig, OllamaConfig, OpenAIConfig]
+
 
 def get_ollama_models():
     try:
@@ -13,10 +15,17 @@ def get_ollama_models():
         print("Please install the langchain-ollama package")
         exit(0)
 
-
-    llm = ChatOllama(base_url=llm_config.url, model=llm_config.chat_model)
-    if llm_config.embeddings_model:
-        embeddings = OllamaEmbeddings(base_url=llm_config.url, model=llm_config.embeddings_model)
+    llm = ChatOllama(
+        base_url=llm_params.url,
+        model=llm_params.chat_model,
+        temperature=0,
+        seed=1234,
+        num_ctx=500,
+    ).configurable_fields(**llm_config)
+    if llm_params.embeddings_model:
+        embeddings = OllamaEmbeddings(
+            base_url=llm_params.url, model=llm_params.embeddings_model
+        )
     else:
         embeddings = None
     return llm, embeddings
@@ -24,24 +33,28 @@ def get_ollama_models():
 
 def get_gemini_models():
     try:
-        from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+        from langchain_google_genai import (
+            ChatGoogleGenerativeAI,
+            GoogleGenerativeAIEmbeddings,
+        )
     except ImportError:
         print("Please install the langchain-google-genai package")
         exit(0)
 
     llm = ChatGoogleGenerativeAI(
-        model=llm_config.chat_model,
-        api_key=llm_config.api_key,
+        model=llm_params.chat_model,
+        api_key=llm_params.api_key,
         temperature=0,
         max_tokens=100,
         timeout=None,
         max_retries=2,
     )
-    if llm_config.embeddings_model:
-        embeddings = GoogleGenerativeAIEmbeddings(model=llm_config.embeddings_model)
+    if llm_params.embeddings_model:
+        embeddings = GoogleGenerativeAIEmbeddings(model=llm_params.embeddings_model)
     else:
         embeddings = None
     return llm, embeddings
+
 
 def get_openai_models():
     try:
@@ -51,27 +64,29 @@ def get_openai_models():
         exit(0)
 
     llm = AzureChatOpenAI(
-        deployment_name=llm_config.chat_model,
+        deployment_name=llm_params.chat_model,
         temperature=0,
         max_tokens=500,
     )
-    if llm_config.embeddings_model:
+    if llm_params.embeddings_model:
         embeddings = AzureOpenAIEmbeddings(
-            azure_deployment=llm_config.embeddings_model,
+            azure_deployment=llm_params.embeddings_model,
         )
     else:
         embeddings = None
     return llm, embeddings
+
 
 def get_transformer_models():
     try:
         from langchain_community.embeddings import Model2vecEmbeddings
     except ImportError:
         print("Please install the langchain-community package")
-        exit(0) 
+        exit(0)
     # TODO: AGI
     embeddings = Model2vecEmbeddings()
     return embeddings
+
 
 match llm_provider:
     case "ollama":
