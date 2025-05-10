60d1b29eebd1555dd2899ff5dccb1430b65a2641
blacksmithop <angstycoder101@gmaii.com>
2025-05-10 15:57:37 +0530

    feat: Activity switcher

--- utils/discord_utils/__init__.py
+++ utils/discord_utils/__init__.py
@@ -1,3 +1,4 @@
 from .embed_creator import create_datetime_embed
 from .ui_kit_elements import FeedbackForm, SummaryTypeView
 from .help_command import HelpCommand, generate_man_page, HelpCommandArgParser
+from .activity_switcher import get_new_activity
---
+++ utils/discord_utils/activity_switcher.py
@@ -0,0 +1,17 @@
+import discord
+from utils import fetch_stoic_quote, StoicQuote
+
+async def get_new_activity():
+    quote_data: StoicQuote = await fetch_stoic_quote()
+    quote = quote_data.data.quote
+    
+    new_activity =discord.Activity(
+            type=discord.ActivityType.listening,
+            name="🤔",
+            state=quote,
+            # assets={
+            #     "large_image": "image3",
+            #     "large_text": "Galaxy View",
+            # },
+        )
+    return new_activity
