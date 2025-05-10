74b11765bad71ce1e7f22647c0809267af33f756
blacksmithop <angstycoder101@gmaii.com>
2025-05-10 11:03:09 +0530

    Change about text

--- cogs/general.py
+++ cogs/general.py
@@ -35,11 +35,11 @@ class General(commands.Cog, name="general"):
         :param context: The hybrid command context.
         """
         embed = discord.Embed(
-            description="Used [Krypton's](https://krypton.ninja) template",
+            description="Experimental bot",
             color=0xBEBEFE,
         )
         embed.set_author(name="Bot Information")
-        embed.add_field(name="Owner:", value="Krypton#7331", inline=True)
+        embed.add_field(name="Owner:", value="blacksmithop", inline=True)
         embed.add_field(
             name="Python Version:", value=f"{platform.python_version()}", inline=True
         )
