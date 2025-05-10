7ff4eae79561220954e81d8f1ddb0d86b8c3d793
blacksmithop <angstycoder101@gmaii.com>
2025-05-10 19:21:09 +0530

    Create basic discord bot website

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                'pure-black': '#000000',
+                'pure-white': '#FFFFFF',
+                'gray-light': '#D1D1D1',
+                'gray-dark': '#333333',
+            }
+        }
+    }
+}
