5bab837ba0eca78ba02521937f5e98d4f0ae96e4
blacksmithop <angstycoder101@gmaii.com>
2025-05-10 10:11:37 +0530

    Remove system prompt (gemini specific issue

--- utils/langchain_utils/summarize.py
+++ utils/langchain_utils/summarize.py
@@ -1,11 +1,11 @@
-from langchain_core.prompts import ChatPromptTemplate
+from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
 from utils import llm, ChainInputInvalid
 from .runnable_parser import get_content_and_metadata
 
 
 prompt = ChatPromptTemplate.from_messages(
     [
-        ("system", "Write a concise summary of the following:```{context}```"),
+        HumanMessagePromptTemplate.from_template("Write a concise summary of the following:```{context}```"),
     ]
 )
 
@@ -15,7 +15,7 @@ summarize_chain = prompt | llm | get_content_and_metadata
 async def summarize_text(text: str, temperature: float = 0.2, top_p: float = 0.9, num_ctx: int = 250):
     try:
         summary_text, usage_metadata = summarize_chain.invoke(
-            {"context": text}, {"configurable": {"temperature": temperature, "num_ctx": num_ctx}} # TODO: Top P, other params
+            {"context": text}, {"configurable": {"temperature": temperature, }}
         )
         return summary_text, usage_metadata
     except Exception as e:
