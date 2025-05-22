a1ee74f59347f222baf68a19bfb655bff5c49c7c
blacksmithop <angstycoder101@gmaii.com>
2025-05-22 17:45:21 +0530

    Update styles, js

--- static/js/index.js
+++ static/js/index.js
@@ -3,11 +3,19 @@ tailwind.config = {
     theme: {
         extend: {
             colors: {
+                'discord-light': '#f6f6f7',
+                'discord-dark': '#36393f',
+                'discord-darker': '#2f3136',
+                'discord-accent': '#5865f2',
+                'discord-accent-hover': '#4752c4',
                 'pure-black': '#000000',
                 'pure-white': '#FFFFFF',
                 'gray-light': '#D1D1D1',
                 'gray-dark': '#333333',
-            }
+            },
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+            },
         }
     }
 }
