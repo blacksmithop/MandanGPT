6cf4a7e4a47b18383f06517e0c7a79104c18102b
blacksmithop <angstycoder101@gmaii.com>
2025-05-23 16:32:05 +0530

    Integrate Langfuse, typo: secret name

--- utils/observability_utils/langfuse_integration.py
+++ utils/observability_utils/langfuse_integration.py
@@ -2,7 +2,7 @@ from langfuse.callback import CallbackHandler
 from os import getenv
 
 langfuse_handler = CallbackHandler(
-    secret_key=getenv("LANGFUSE_SECRET_KEYs"),
+    secret_key=getenv("LANGFUSE_SECRET_KEY"),
     public_key=getenv("LANGFUSE_PUBLIC_KEY"),
     host=getenv("LANGFUSE_HOST")
 )
